3906f56795eecd33305b6534158b695938cdb5d4
Geoffrey <geoffrey.pruvost@hotmail.com>
2016-03-18 11:28:09 +0100

    version 2.0

---
+++ assets/php/contact.php
@@ -0,0 +1,33 @@
+<?php
+
+if(isset($_POST['message'])){
+
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+    
+	
+	$to      = 'geoffrey.pruvost62@gmail.com';
+	$subject = 'Site Contact Form';
+
+	$headers = 'From: '. $email . "\r\n" .
+    'Reply-To: '. $email . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+	$status = mail($to, $subject, $message, $headers);
+
+	if($status == TRUE){	
+		$res['sendstatus'] = 'done';
+	
+		//Edit your message here
+		$res['message'] = 'Message envoyé.';
+    }
+	else{
+		$res['message'] = 'Une erreur est survenue, contacter geoffrey.pruvost@hotmail.com';
+	}
+	
+	
+	echo json_encode($res);
+}
+
+?>
